deab66835c60451bb9a44065d80cb89a35556a4d
bvrbryn445 <beaverbryanantipolo@gmail.com>
2022-08-21 00:25:49 -0400

    add merge sort

--- recursion/merge-sort.js
+++ recursion/merge-sort.js
@@ -4,22 +4,20 @@ export default function mergeSort(arr) {
 	} else {
 		// split the initial array into half
 		const indexToSplit = Math.ceil(arr.length / 2);
-		console.log(indexToSplit);
-		const firstHalf = mergeSort(arr.slice(0, indexToSplit - 1));
+		const firstHalf = mergeSort(arr.slice(0, indexToSplit));
 		const secondHalf = mergeSort(arr.slice(indexToSplit));
 
 		const finalProduct = [];
-		
 		// individually compare each element from both sorted arrays
 		// in order to sort them
 		for (let i = 0; i < arr.length; i++) {
-			if ((firstHalf[0] < secondHalf[0]) && firstHalf[0]) {
+			if ((firstHalf[0] <= secondHalf[0] || !secondHalf.length)) {
 				finalProduct.push(firstHalf.shift());
-			} else {
+			} else if((firstHalf[0] > secondHalf[0]  || !firstHalf.length)) {
 				finalProduct.push(secondHalf.shift());
 			}
 		}
-		console.log(finalProduct);
+		// console.log(finalProduct);
 		return finalProduct;
 	}
 }
--- recursion/tests/merge-sort.test.js
+++ recursion/tests/merge-sort.test.js
@@ -1,36 +1,36 @@
 import mergeSort from '../merge-sort.js';
 
-test.only('sort a short amount of elements array', () => {
+test('sort a short amount of elements array', () => {
 	const unsortedArr = [33, 11, 22, 1];
 	const sortedArr = [1, 11, 22, 33];
 	expect(mergeSort(unsortedArr))
-		.toBe(sortedArr);
+		.toStrictEqual(sortedArr);
 })
 
 test('sort even numbered length array', () => {
 	const unsortedArr = [4, 6, 3, 2, 7, 9, 1, 8];
 	const sortedArr = [1, 2, 3, 4, 6, 7, 8, 9];
 	expect(mergeSort(unsortedArr))
-		.toBe(sortedArr);
+		.toStrictEqual(sortedArr);
 })
 
 test('sort odd numbered length array', () => {
 	const unsortedArr = [2, 7, 5, 3, 9, 1, 6];
 	const sortedArr = [1, 2, 3, 5, 6, 7, 9];
 	expect(mergeSort(unsortedArr))
-		.toBe(sortedArr);
+		.toStrictEqual(sortedArr);
 });
 
 test('sort duplicated elements', () => {
 	const unsortedArr = [2, 6, 5, 3, 6, 1];
 	const sortedArr = [1, 2, 3, 5, 6, 6];
 	expect(mergeSort(unsortedArr))
-		.toBe(sortedArr);
+		.toStrictEqual(sortedArr);
 });
 
 test('sort a large input size array', () => {
 	const unsortedArr = [21, 24, 9, 31, 32, 15, 25, 11, 19, 7, 2, 12, 17, 3, 28, 29, 22, 6, 8, 13, 26, 16, 27, 1, 14, 18, 5, 4, 30, 10, 23, 20];
 	const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
 	expect(mergeSort(unsortedArr))
-		.toBe(sortedArr);
+		.toStrictEqual(sortedArr);
 })
